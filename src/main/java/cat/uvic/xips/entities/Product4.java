d72c595b98920d227f90b6e7a6d2cf27aaac0c88
afcasco <7701272+afcasco@users.noreply.github.com>
2024-05-09 10:50:34 +0200

    remove duplicatin

---
+++ src/main/java/cat/uvic/xips/entities/Product4.java
@@ -0,0 +1,26 @@
+package cat.uvic.xips.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name="products")
+public class Product4 {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
+    private ProductType productType;
+    private String username;
+
+}
