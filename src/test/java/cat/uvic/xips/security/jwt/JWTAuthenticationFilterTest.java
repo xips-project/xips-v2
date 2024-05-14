88767c3c545176fb18ff7fbe030857359ee5e42a
Alex <7701272+afcasco@users.noreply.github.com>
2024-05-14 15:38:06 +0200

    removed reflection from authenticationfilter tests and use jwtservice mock instead

--- src/test/java/cat/uvic/xips/security/jwt/JWTAuthenticationFilterTest.java
+++ src/test/java/cat/uvic/xips/security/jwt/JWTAuthenticationFilterTest.java
@@ -6,23 +6,26 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.IOException;
-import java.lang.reflect.Field;
 import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
-
+@ExtendWith(MockitoExtension.class)
 class JWTAuthenticationFilterTest {
 
     private JWTAuthenticationFilter jwtAuthenticationFilter;
+
+
+    @Mock
     private JWTService jwtService;
     private UserDetails mockUserDetails;
 
@@ -37,29 +40,30 @@ class JWTAuthenticationFilterTest {
 
     @BeforeEach
     void setUp() throws IllegalAccessException, NoSuchFieldException {
-        MockitoAnnotations.openMocks(this);
-        jwtService = new JWTService();
+
         mockUserDetails = new User("testUser", "testPassword", Collections.emptyList());
         jwtAuthenticationFilter = new JWTAuthenticationFilter(jwtService, userDetails -> mockUserDetails);
 
-        Field jwtSecretField = JWTService.class.getDeclaredField("jwtSecret");
+       /* Field jwtSecretField = JWTService.class.getDeclaredField("jwtSecret");
         jwtSecretField.setAccessible(true);
-        jwtSecretField.set(jwtService, "bXktYXBwLXN1cGVyLXNlY3JldC1mb3ItdGVzdHMtb25seQ==");
+        jwtSecretField.set(jwtService, "bXktYXBwLXN1cGVyLXNlY3JldC1mb3ItdGVzdHMtb25seQ==");*/
 
     }
 
     @Test
     void testDoFilterInternal() throws ServletException, IOException {
-        when(mockRequest.getHeader(HttpHeaders.AUTHORIZATION)).thenReturn("Bearer " + jwtService.getToken(mockUserDetails));
+        String mockToken = "mockToken";
+        when(mockRequest.getHeader(HttpHeaders.AUTHORIZATION)).thenReturn("Bearer " + mockToken);
 
         jwtAuthenticationFilter.doFilterInternal(mockRequest, mockResponse, mockFilterChain);
-
         verify(mockFilterChain, times(1)).doFilter(mockRequest, mockResponse);
     }
 
     @Test
     void testGetTokenFromRequest() {
-        when(mockRequest.getHeader(HttpHeaders.AUTHORIZATION)).thenReturn("Bearer " + jwtService.getToken(mockUserDetails));
+        String mockToken = "mockToken";
+        when(jwtService.getToken(any())).thenReturn(mockToken);
+        when(mockRequest.getHeader(HttpHeaders.AUTHORIZATION)).thenReturn("Bearer " + mockToken);
 
         String token = jwtAuthenticationFilter.getTokenFromRequest(mockRequest);
 
