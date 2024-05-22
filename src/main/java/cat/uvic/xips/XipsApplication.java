9097fa1d138c7c1e516d52caa8693ae82f6fa88f
Alex <7701272+afcasco@users.noreply.github.com>
2024-05-22 16:52:32 +0200

    remove database initializng for testing localy

--- src/main/java/cat/uvic/xips/XipsApplication.java
+++ src/main/java/cat/uvic/xips/XipsApplication.java
@@ -1,28 +1,15 @@
 package cat.uvic.xips;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-public class XipsApplication implements CommandLineRunner {
-
-	//private final DataInitializer dataInitializer;
-
-   /* public XipsApplication(DataInitializer dataInitializer) {
-        this.dataInitializer = dataInitializer;
-    }*/
+public class XipsApplication  {
 
     public static void main(String[] args)  {
 		SpringApplication.run(XipsApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		//dataInitializer.initializeUsers();
-		//dataInitializer.initializeProducts();
-		//dataInitializer.initializeRatings();
-	}
 }
